ee13e4c1591998b6d131c8b2f60b09838d0e9447
Ashley <ashhamp27@gmail.com>
2016-04-06 10:34:36 -0400

    fixes hound errors

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 require_relative '../factories/users'
 feature "user authentication" do
 # [ ] As a prospective user
-#     I want to create an account
-#     So that I can post items and review them
+# I want to create an account
+# So that I can post items and review them
 #
 # [ ] As an unauthenticated user
-#     I want to sign in
-#     So that I can post items and review them
+# I want to sign in
+# So that I can post items and review them
 #
 # [ ] As an authenticated user
-#     I want to sign out
-#     So that no one else can post items or reviews on my
-#     behalf
+# I want to sign out
+# So that no one else can post items or reviews on my
+# behalf
 #
 # [ ] As an authenticated user
-#     I want to update my information
-#     So that I can keep my profile up to date
+# I want to update my information
+# So that I can keep my profile up to date
 #
 # [ ] As an authenticated user
-#     I want to delete my account
-#     So that my information is no longer retained by the app
+# I want to delete my account
+# So that my information is no longer retained by the app
 
   describe "user creates an account" do
-      user = FactoryGirl.create(:user)
+  user = FactoryGirl.create(:user)
 
     scenario "user enters valid data to create account" do
       visit "/users/sign_up"
@@ -45,18 +45,17 @@ feature "user authentication" do
       fill_in "Password", with: user.password
       fill_in "Password confirmation", with: "Ooga Booga"
       click_on "Sign Up"
-            save_and_open_page
 
       expect(page).to_not have_content "Welcome! You have signed up successfully."
-      expect(page).to have_content "Password confirmation doesn't match Password"
+      expect(page).to have_content "confirmation doesn't match Password"
     end
     #
     # scenario "user enters invalid data to create account" do
-    #   visit "/users/sign_up"
+    # visit "/users/sign_up"
     #
-    #   click_on "Sign Up"
+    # click_on "Sign Up"
     #
-    #   expect(page).to_not have_content "Username can't be blank. Email can't be blank. Password can't be blank."
+    # expect(page).to_not have_content "Username can't be blank."
     # end
   end
 end
