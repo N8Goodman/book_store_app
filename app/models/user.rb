33d7e4629383eeb6c098b6a4b157c40f20700503
Ann Colvin <anncolvinpierce@gmail.com>
2016-04-07 13:18:05 -0400

    updated validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, format: { with: /\w+@\w+\.\w+/ }
   validates :encrypted_password, presence: true, length: { minimum: 8 }
-  validates :user_name, presence: true, length: { minimum: 3, too_short:
-  "User name must have at least 3 characters." }, length: { maximum: 20, too_long:
-  "User name can have a maximum of 20 characters." }
+  validates :user_name, presence: true, length: { in: 3..20, message: "User name must be between 3 and 20 characters." }
   validates :encrypted_password, presence: true, length: { minimum: 8 }
 end
