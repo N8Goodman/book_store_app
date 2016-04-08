2654220c65466e72ea2258b43adfe106f9183496
Ashley <ashhamp27@gmail.com>
2016-04-08 13:51:39 -0400

    adds test to confirm deletion of user's bookstores on user account deletion

--- spec/features/user_deletes_account_spec.rb
+++ spec/features/user_deletes_account_spec.rb
@@ -6,14 +6,37 @@ require 'rails_helper'
 
 feature "user deletes their account" do
 
-  let(:user1) { FactoryGirl.create(:user) }
+  let!(:user1) { FactoryGirl.create(:user) }
+  let!(:user2) { FactoryGirl.create(:user, user_name: "heyyythere") }
+  let!(:bookstore1) { FactoryGirl.create(:bookstore, user: user1) }
+  let!(:bookstore2) { FactoryGirl.create(:bookstore, user: user1) }
+  let!(:bookstore3) { FactoryGirl.create(:bookstore, user: user2) }
 
   scenario "user deletes account successfully" do
     sign_in(user1)
+
     click_on("Update Profile")
 
     click_on "Cancel my account"
 
     expect(page).to have_content "Bye! Your account has been successfully cancelled. We hope to see you again soon."
+  end
+
+  scenario "user deletes account and all bookstores they created" do
+    sign_in(user1)
+
+    expect(page).to have_content bookstore1.name
+    expect(page).to have_content bookstore2.name
+    expect(page).to have_content bookstore3.name
+
+    click_on("Update Profile")
+
+    click_on "Cancel my account"
+
+    visit bookstores_path
+
+    expect(page).to have_content bookstore3.name
+    expect(page).to_not have_content bookstore1.name
+    expect(page).to_not have_content bookstore2.name
   end
 end
