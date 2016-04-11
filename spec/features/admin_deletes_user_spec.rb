3bf983df7c29ca79e7ad21c420318dc7d8f6ba3f
Ashley <ashhamp27@gmail.com>
2016-04-11 16:32:35 -0400

    resolves hound issues

--- spec/features/admin_deletes_user_spec.rb
+++ spec/features/admin_deletes_user_spec.rb
@@ -10,7 +10,6 @@ feature "Admin deletes user" do
   let!(:user2) { FactoryGirl.create(:user) }
   let!(:user3) { FactoryGirl.create(:user) }
 
-
   scenario "Admin deletes user" do
     sign_in(user1)
     visit admin_users_path
@@ -19,11 +18,11 @@ feature "Admin deletes user" do
     expect(page).to have_content user2.user_name
     expect(page).to have_content user3.user_name
 
-    click_on ("delete#{user2.id}")
+    click_on "delete#{user2.id}"
 
     expect(page).to_not have_content user2.user_name
     expect(page).to have_content user1.user_name
     expect(page).to have_content user3.user_name
-    expect(page).to have_content("User deleted successfully.")
+    expect(page).to have_content "User deleted successfully."
   end
 end
--- spec/features/admin_sees_list_of_users_spec.rb
+++ spec/features/admin_sees_list_of_users_spec.rb
@@ -10,7 +10,6 @@ feature "Admin sees list of users" do
   let!(:user2) { FactoryGirl.create(:user) }
   let!(:user3) { FactoryGirl.create(:user) }
 
-
   scenario "Admin sees list of users" do
     sign_in(user1)
     visit admin_users_path
