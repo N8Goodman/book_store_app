9c5177e7e2cd6127d92a0470079c5f960f6f4383
Ashley <ashhamp27@gmail.com>
2016-04-11 21:30:53 -0400

    resolves hound issues

--- spec/models/twitter_client_spec.rb
+++ spec/models/twitter_client_spec.rb
@@ -3,26 +3,35 @@ require 'rails_helper'
 RSpec.describe TwitterClient, type: :model do
   describe "#post_tweet(bookstore)" do
     let!(:twitter_client) { TwitterClient.new }
-    let!(:bookstore) { FactoryGirl.create(:bookstore, name: "Bookstore Awesome", city: "Boston", state: "Massachusetts") }
-    let(:message) {   message = "#{bookstore.name}, " + "#{bookstore.city}, #{bookstore.state} was just added! Check " +
-    "it out at literalist.herokuapp.com/bookstores/#{bookstore.id}" }
 
-     it "posts a tweet" do
-       VCR.use_cassette 'model/twitter_client' do
-          response = twitter_client.post_tweet(message)
+    let!(:bookstore) { FactoryGirl.create(
+      :bookstore,
+      name: "Bookstore Awesome",
+      city: "Boston",
+      state: "Massachusetts")
+    }
 
-          expect(response).to_not eq nil
-          expect(response).to be_a(Twitter::Tweet)
-          expect(response.text).to include("#{bookstore.name}, #{bookstore.city}, #{bookstore.state} was just added!")
-       end
-     end
-
-     it "handles errors in posting tweet and returns nil" do
-       VCR.use_cassette 'model/twitter_client_fail' do
-          response = twitter_client.post_tweet(message)
-
-          expect(response).to eq nil
-       end
-     end
+    let(:message) do
+      "#{bookstore.name}, #{bookstore.city}, #{bookstore.state} " +
+      "was added! Check it out at: " + "literalist.herokuapp.com/bookstores/#{bookstore.id}"
     end
+
+   it "posts a tweet" do
+     VCR.use_cassette 'model/twitter_client' do
+        response = twitter_client.post_tweet(message)
+
+        expect(response).to_not eq nil
+        expect(response).to be_a(Twitter::Tweet)
+        expect(response.text).to include("#{bookstore.name}, #{bookstore.city}, #{bookstore.state} was just added!")
+     end
+   end
+
+   it "handles errors in posting tweet and returns nil" do
+     VCR.use_cassette 'model/twitter_client_fail' do
+        response = twitter_client.post_tweet(message)
+
+        expect(response).to eq nil
+     end
+   end
   end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,8 +21,10 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.default_cassette_options = { record: :new_episodes }
-  config.filter_sensitive_data('<TWITTER_API_KEY>') {
-    ENV['TWITTER_API_KEY'] }
-  config.filter_sensitive_data('<TWITTER_ACCESS_TOKEN>') {
-    ENV['TWITTER_ACCESS_TOKEN'] }
+  config.filter_sensitive_data('<TWITTER_API_KEY>') do
+    ENV['TWITTER_API_KEY']
+  end
+  config.filter_sensitive_data('<TWITTER_ACCESS_TOKEN>') do
+    ENV['TWITTER_ACCESS_TOKEN']
+  end
 end
