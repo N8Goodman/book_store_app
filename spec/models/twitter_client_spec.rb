b14d5697fdfe723059fa443c457d46f31574d844
Ashley <ashhamp27@gmail.com>
2016-04-12 15:16:04 -0400

    fix indentation?

--- spec/models/twitter_client_spec.rb
+++ spec/models/twitter_client_spec.rb
@@ -2,33 +2,34 @@ require 'rails_helper'
 
 RSpec.describe TwitterClient, type: :model do
   describe "#post_tweet(bookstore)" do
+    let!(:twitter_client) { TwitterClient.new }
+    let!(:bookstore) do FactoryGirl.create(
+      :bookstore,
+      name: "Bookstore Awesome",
+      city: "Boston",
+      state: "Massachusetts")
+    end
 
-  let!(:twitter_client) { TwitterClient.new }
-  let!(:bookstore) do FactoryGirl.create(
-    :bookstore,
-    name: "Bookstore Awesome",
-    city: "Boston",
-    state: "Massachusetts")
-  end
-
-  let(:message) do
-    "#{bookstore.name}, #{bookstore.city}, #{bookstore.state} was added! Check it out at: literalist.herokuapp.com/bookstores/#{bookstore.id}"
-  end
+    let(:message) do
+      <<-MSG
+        #{bookstore.name}, #{bookstore.city}, #{bookstore.state} was added! Check it out at: literalist.herokuapp.com/bookstores/#{bookstore.id}
+      MSG
+    end
 
     it "posts a tweet" do
-     VCR.use_cassette 'model/twitter_client' do
-       response = twitter_client.post_tweet(message)
-       expect(response).to_not eq nil
-       expect(response).to be_a(Twitter::Tweet)
-       expect(response.text).to include(message)
-     end
-   end
+      VCR.use_cassette 'model/twitter_client' do
+        response = twitter_client.post_tweet(message)
+        expect(response).to_not eq nil
+        expect(response).to be_a(Twitter::Tweet)
+        expect(response.text).to include(message)
+      end
+    end
 
-   it "handles errors in posting tweet and returns nil" do
-     VCR.use_cassette 'model/twitter_client_fail' do
-       response = twitter_client.post_tweet(message)
-       expect(response).to eq nil
-     end
-   end
- end
+    it "handles errors in posting tweet and returns nil" do
+      VCR.use_cassette 'model/twitter_client_fail' do
+        response = twitter_client.post_tweet(message)
+        expect(response).to eq nil
+      end
+    end
+  end
 end
