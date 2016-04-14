ccc03be6f2a75049b3846373ad2a6abbe33d6a15
Ann Colvin <anncolvinpierce@gmail.com>
2016-04-14 14:18:17 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ unless admin_user1.present?
   admin_user1 = User.create(
     user_name: "history_buff",
     email: "historybuff40@example.com",
-    password: "inthepast",
-    password_confirmation: "inthepast",
+    password: ENV["ADMIN_1_KEY"],
+    password_confirmation: ENV["ADMIN_1_KEY"],
     admin: true
 )
 end
@@ -14,8 +14,8 @@ unless admin_user2.present?
   admin_user2 = User.create(
     user_name: "admin1",
     email: "awesomeadmin@example.com",
-    password: "bestadmineva",
-    password_confirmation: "bestadmineva",
+    password: ENV["ADMIN_2_KEY"],
+    password_confirmation: ENV["ADMIN_2_KEY"],
     admin: true
 )
 end
@@ -82,7 +82,7 @@ end
 
 bookstore1 = Bookstore.find_or_create_by(
   name: "Best Books",
-  address: "101 Main Street",
+  address: "101 Main St.",
   city: "Boston",
   state: "MA",
   zip_code: "02111",
@@ -91,7 +91,7 @@ bookstore1 = Bookstore.find_or_create_by(
 
 bookstore2 = Bookstore.find_or_create_by(
   name: "Book Nook",
-  address: "777 Main Street",
+  address: "777 Main St.",
   city: "Edgartown",
   state: "MA",
   zip_code: "02539",
@@ -109,7 +109,7 @@ bookstore3 = Bookstore.find_or_create_by(
 
 bookstore4 = Bookstore.find_or_create_by(
   name: "Anderson's Book Haven",
-  address: "16 Oak Street",
+  address: "16 Oak St.",
   city: "Harrisburg",
   state: "PA",
   zip_code: "17112",
@@ -150,6 +150,114 @@ bookstore8 = Bookstore.find_or_create_by(
   state: "MA",
   zip_code: "02138",
   user: non_admin_user4
+)
+
+bookstore9 = Bookstore.find_or_create_by(
+  name: "Chocolate & Pages",
+  address: "19 Berry St.",
+  city: "Helena",
+  state: "MT",
+  zip_code: "59601",
+  user: admin_user1
+)
+
+bookstore10 = Bookstore.find_or_create_by(
+  name: "Litterally by Design",
+  address: "731 Calvin Dr.",
+  city: "Phoenix",
+  state: "AZ",
+  zip_code: "85001",
+  user: non_admin_user6
+)
+
+bookstore11 = Bookstore.find_or_create_by(
+  name: "Craven's Edge",
+  address: "7 South Water St.",
+  city: "Rotunda West",
+  state: "FL",
+  zip_code: "33947",
+  user: non_admin_user5
+)
+
+bookstore12 = Bookstore.find_or_create_by(
+  name: "From Austen to Zahn",
+  address: "43 Winter Rd.",
+  city: "Cambridge",
+  state: "MA",
+  zip_code: "02138",
+  user: admin_user2
+)
+
+bookstore13 = Bookstore.find_or_create_by(
+  name: "The Written Word",
+  address: "538 Kay Ln.",
+  city: "Cambridge",
+  state: "MA",
+  zip_code: "02142",
+  user: non_admin_user2
+)
+
+bookstore14 = Bookstore.find_or_create_by(
+  name: "Argyle Books",
+  address: "45 Paisley Way",
+  city: "Athens",
+  state: "GA",
+  zip_code: "30602",
+  user: non_admin_user3
+)
+
+bookstore15 = Bookstore.find_or_create_by(
+  name: "Under Cover",
+  address: "87 East 17th St.",
+  city: "Boston",
+  state: "MA",
+  zip_code: "02115",
+  user: non_admin_user6
+)
+
+bookstore16 = Bookstore.find_or_create_by(
+  name: "Intellectual Investments",
+  address: "34 Lighhouse Rd.",
+  city: "Cambridge",
+  state: "MA",
+  zip_code: "02138",
+  user: non_admin_user4
+)
+
+bookstore17 = Bookstore.find_or_create_by(
+  name: "Comedy of Errors",
+  address: "65 Lake St.",
+  city: "Boston",
+  state: "MA",
+  zip_code: "02111",
+  user: admin_user1
+)
+
+bookstore18 = Bookstore.find_or_create_by(
+  name: "Colonial Books",
+  address: "382 Summer St.",
+  city: "Modesto",
+  state: "CA",
+  zip_code: "95351",
+  user: non_admin_user6
+)
+
+bookstore19 = Bookstore.find_or_create_by(
+  name: "Anachronisms and Non-sequiturs",
+  address: "42 River Rd.",
+  city: "Ackworth",
+  state: "IA",
+  zip_code: "50001",
+  user: non_admin_user5
+)
+
+bookstore20 = Bookstore.find_or_create_by(
+  name: "Absurd Literature",
+  address: "7 Pine St.",
+  city: "Nashville",
+  state: "TN",
+  zip_code: "37115",
+  user: admin_user2
 )
 
 b1_review1 = Review.find_or_create_by(
