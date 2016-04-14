8dffbfb8fd45295cb5b1d12a312ff0141630d1fe
Nathan Goodman <Nate@Nathans-MacBook-Air.local>
2016-04-14 14:34:30 -0400

    add functionality for voting in ajax AND ruby

--- spec/features/user_votes_on_a_review_spec.rb
+++ spec/features/user_votes_on_a_review_spec.rb
@@ -48,8 +48,6 @@ feature "user votes on a review" do
 
     click_link bookstore1.name
     click_on "+1"
-    save_and_open_page
-
 
     expect(page).to have_content "You must be signed in"
   end
